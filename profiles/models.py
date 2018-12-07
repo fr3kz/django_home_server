3041bdd508ce31e856454dd5df16363695b68b42
fr3kz <tektmap1@gmail.com>
2018-12-07 11:45:09 +0200

    complete

--- profiles/models.py
+++ profiles/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
+
 class File(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)      
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)        
     upload = models.FileField(upload_to='files/')
     usr = models.IntegerField(blank=True, null=True)
-    
     def __str__(self):
-        return "plik"
+        return "plik"    
--- profiles/views.py
+++ profiles/views.py
@@ -1,22 +1,46 @@
 from django.shortcuts import render,redirect
 from django.core.files.storage import FileSystemStorage
 from .models import File as FileModel
-# Create your views here.s
+from django.contrib.auth.models import User
+from django.contrib import auth
+# Create your views here
+
+def index(request):
+    arturs_files = FileModel.objects.filter(user='3')
+    mamas_files = FileModel.objects.filter(user='2')
+    tata_files = FileModel.objects.filter(user='1')
+    print(arturs_files)
+    context = {
+        'artur_files':arturs_files,
+        'mama_files':mamas_files,
+        'tata_files':tata_files,
+    }
+    return render(request, "profiles/index.html",context)
+
+
 def upload(request):
     if request.method == 'POST':
         myfile = request.FILES['myfile']
         fs = FileSystemStorage("files/")
         filename = fs.save(myfile.name, myfile)
-        fileModel = FileModel(upload=myfile.name)
+        fileModel = FileModel(upload=myfile.name,user=request.user)
         fileModel.save()
         fs.url(filename)
         return redirect('index')
     else:
         return render(request, 'profiles/upload.html')
 
-
-def index(request):
-    return render(request, "profiles/index.html")
-
 def login(request):
-    return render(request, "profiles/index.html")
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']    
+        
+        user = auth.authenticate(username=username,password=password)
+
+        if user is not None:
+            auth.login(request,user)
+            return redirect('index')
+        else:
+            return redirect('loguj')
+    else:
+        return render(request, "profiles/login.html")
